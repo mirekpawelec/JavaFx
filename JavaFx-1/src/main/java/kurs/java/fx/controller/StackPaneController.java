0bb22782ecf427a006e17354e2d6afa85c0050d6
Mirek Pawelec <mirek.pawelec@wp.pl>
2018-10-17 23:12:32 +0200

    added various way the handler of events in java

--- JavaFx-1/src/main/java/kurs/java/fx/controller/StackPaneController.java
+++ JavaFx-1/src/main/java/kurs/java/fx/controller/StackPaneController.java
@@ -2,8 +2,11 @@ package kurs.java.fx.controller;
 
 import javax.sound.midi.Soundbank;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 
 public class StackPaneController {
 
@@ -14,10 +17,31 @@ public class StackPaneController {
 		System.out.println("Jestem w kontrolerze!");
 	}
 	
-	/* metoda wywo³ywana zaraz po konstruktorze, gwarantuje, ¿e przyciski z formy zosta³y wstrzykniête i mo¿emy je modyfikowaæ */
 	@FXML
 	void initialize() {
-		button.setText("Nowa nazwa");		// je¿eli wywo³amy metode w konstruktorze otrzymamy NullPointnerException, poniewa¿ podczas inicjalizacji konstruktora pola kontrolera jeszcze nie zosta³y zainicjowane
+		button.setText("Nowa nazwa");
+		
+		/* dodanie obs³ugi eventów w java, poza fxml */
+		// 1. dodanie EventHandlera, w którym jako typ wskazujemy rodzaj zdarzenia
+		// 2. dodanie implementacji metody handle()
+		EventHandler<ActionEvent> handler = new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				System.out.println("To jest handler");
+			}
+		};
+		button.addEventHandler(ActionEvent.ACTION, handler);	// w ten sposób mo¿emy zdefiniowaæ wiele handlerów
+	
+		/* implementacja obs³ugi eventu za pomoc¹ lambda */
+		EventHandler<MouseEvent> mouseHandler = e -> System.out.println("To jest Mouse Handler");
+		button.addEventHandler(MouseEvent.MOUSE_ENTERED, mouseHandler);
+		
+		/* usuwanie handlerów */
+		button.removeEventHandler(MouseEvent.MOUSE_ENTERED, mouseHandler);
+		
+		/* Trzeci sposób obs³ugi eeventów. WA¯NE! Tym sposobem mo¿emy zdefiniowaæ tylko jeden handler */
+		button.setOnAction(e -> {
+			System.out.println("To jest setOnActionHandler ");
+		});
 	}
 	
 }
